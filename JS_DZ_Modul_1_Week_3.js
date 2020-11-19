83e856cf7f30a09ca6daa48603cb5e81e2c552e3
Edurd <yemelyanov.eduard@gmail.com>
2020-11-19 14:23:34 +0200

    Completed JS_DZ_Modul_1_Week_3

--- JS_DZ_Modul_1_Week_3.js
+++ JS_DZ_Modul_1_Week_3.js
@@ -49,6 +49,14 @@
     nuberArea2.innerHTML='У числа '+userNumbers+' следущие делители: '+rezultArray.toString();
   }
 
+  function numberLength(someNumbers) {
+    let i=0, j=someNumbers;
+    while (j>0) {
+      j =  Math.floor(j/10);
+      ++i;
+    }
+    return i;
+  }
 
   function digitСapacity (){
     let userNumbers = prompt("Введите целое число", "234567856");    //  Самый простой вариант:
@@ -57,11 +65,7 @@
       nuberArea3.innerHTML= 'Нужно ввести целое число, сэр!';
       return false;
     }
-    let i=0, j=userNumbers;
-    while (j>0) {
-      j =  Math.floor(j/10);
-      ++i;
-    }
+    let i=numberLength(userNumbers);
     nuberArea3.innerHTML='В числе '+userNumbers+': '+i+' цифр.';
   }
 
@@ -81,13 +85,40 @@
     nuberArea4.innerHTML='В массиве чисел: '+arrayOfStrings.toString()+'--- четных чисел: '+c+', не четных: '+nc+', положительных:'+k+', отрицательных:'+l+', равных нулю:'+lo ;
   }
 
-  function arrayNumbers (){
+  function loopСalculator (){
+    let rezult_=0;
     do {
-      let userNumbers = prompt("Введите  числа, через пробел, допускается ввод отрицательных чисел, вещественных чисел через точку и ноль", "18 30 -12 456 0 -45 75 876.5 -1 23");
-      var arrayOfStrings = userNumbers.split(' ');
+      var userNumbers = prompt("Введите два числа и математический оператор между ними", "32 >> 4");
+      if(userNumbers) {
+        alert(userNumbers+' = '+eval(userNumbers) );
+        rezult_=userNumbers;
+      }
+    } while (userNumbers);
+    nuberArea5.innerHTML='Цикл остановлен:) ' + rezult_+' = '+eval(rezult_);
+  }
 
-} while (userNumbers);
+  function numberShift() {
+    let userNumbers = prompt("Введите целое число и число сдвига цифр через запятую", "23455677888, 5"); //  Не исползуя(почти) свойсва строк и массивов JS)
+    if (!userNumbers) return false;
+    let arrayOfStrings = userNumbers.split(',');
+    let i =numberLength(arrayOfStrings[0]);
+    let n = arrayOfStrings[1], rezult_=arrayOfStrings[0], m;
+    for (var k = 0; k < n; k++) {
+      m = Math.floor(rezult_/10**(i-1));
+      rezult_=((rezult_ - m*(10**(i-1)))*10 + m);
+    }
+    nuberArea6.innerHTML='Ваше число ' + arrayOfStrings[0] +' сдвинутое на '+ arrayOfStrings[1] + ' знаков = '+ rezult_;
+  }
 
-
-    nuberArea4.innerHTML='В массиве чисел: '+arrayOfStrings.toString()+'--- четных чисел: '+c+', не четных: '+nc+', положительных:'+k+', отрицательных:'+l+', равных нулю:'+lo ;
+  function numberShift1() {                                                                                   // Используя мощь JS)
+    let userNumbers = prompt("Введите целое число и число сдвига цифр через запятую", "456jfhjhc65, 6");
+    if (!userNumbers) return false;
+    let arrayOfStrings = userNumbers.split(',');
+    let arrayNumber = arrayOfStrings[0].split('');
+    let n = arrayOfStrings[1], rezult_=0, m;
+    for (var k = 0; k < n; k++) {
+      m = arrayNumber.shift();
+      rezult_ = arrayNumber.push(m);
+    }
+    nuberArea7.innerHTML='Ваше число ' + arrayOfStrings[0] +' сдвинутое на '+ arrayOfStrings[1] + ' знаков = '+ arrayNumber.join('');
   }
