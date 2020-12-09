c04457db7f987f37c077bf55a2f7b105f87c6b75
Eduard <yemelyanov.eduard@gmail.com>
2020-12-09 20:22:56 +0200

    ...

--- JS_DZ_2_Modul_1_Week_6.js
+++ JS_DZ_2_Modul_1_Week_6.js
@@ -36,12 +36,9 @@
 
     this.properFraction = function(){
       if (this.z[0]>this.z[1]) {
-        console.log(Math.floor(this.z[0]/this.z[1]));
         return Math.floor(this.z[0]/this.z[1]) +'*'+ (this.z[0]-Math.floor(this.z[0]/this.z[1])*this.z[1]) + '/'+ this.z[1];
       }else {
-        console.log(Math.floor(this.z[0]/this.z[1]));
         return  this.z[0]+ '/'+ this.z[1];
-
       }
     }
 
@@ -51,22 +48,21 @@
       this.z[0] = numerator/nod;
       this.z[1] = this.nok/nod;
       return this.properFraction();
-    }  //console.log(this.z[0],this.z[1]);
-
+    }
 
     this.subtractionFractions = function() {
       let numerator = Math.abs(+this.nok/this.x[1]*this.x[0] - this.nok/this.y[1]*this.y[0]);
+      if (numerator==0) return '0' ;
       let nod = grComFactor(numerator,this.nok);
       this.z[0] = numerator/nod;
       this.z[1] = this.nok/nod;
       if (this.x[0]/this.x[1]<this.y[0]/this.x[1]) return '-'+ this.properFraction() ;
-      console.log(this.z[0],this.z[1]);
-      return this.properFraction() ;
-      //
+
+      return this.properFraction();
     }
   }
 
-  let r = new CommonFractions([3,5],[12,5]);
+  let r = new CommonFractions([2,4],[4,8]);
   r.additionFractions();
   console.log(r.subtractionFractions());
 
