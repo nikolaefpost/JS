49092237f03ae7a574f774762b6112dbab8810ee
Eduard <nikolaef.post@gmail.com>
2021-01-13 13:36:21 +0200

    ...

--- JS_DZ_2_Modul_2_Week_10/JS_DZ_2_Modul_2_Week_10.js
+++ JS_DZ_2_Modul_2_Week_10/JS_DZ_2_Modul_2_Week_10.js
@@ -25,6 +25,15 @@
     circumFerence(){
       return Math.PI*this._radius*2;
     }
+
+    showInfo(){
+      return  this.str = '<p style="border-radius: 50%; border: 1px solid black; line-height:'+this.diameter+
+      'px; overflow: hidden; width: '+this.diameter+'px; height:'+this.diameter +
+      'px;">-------------------------------</p> Радиус окружности: R= '+this.radius+'px; <br>'+
+      'Диаметр окружности: D= ' + this.diameter + 'px; <br> Площадь окружности: S= ' + this.areaCircle()+
+       'px2; <br> Длинна окружности: L= ' + this.circumFerence()+ 'px;';
+       
+    }
   }
 // --------------------------------------------------------------------- 2-e задание --------------------------------------------------------------------------
   class HtmlElement {
@@ -71,10 +80,9 @@
       str+='>'+this.text;
       if (this.attachment.length>0){
         for (var i = 0; i < this.attachment.length; i++) {
-        str+=this.attachment[i].getHtml();
+          str+=this.attachment[i].getHtml();
+        }
       }
-    }
-
       if(!this.self_сlosing) str+='</'+this.name_+'>'
       return str;
     }
@@ -83,48 +91,23 @@
 
   window.onload = function () {
 
-
     click101.onclick = function () {
       let userCircle = new Circle();
       let inputvalue = document.getElementById('input101').value;
       userCircle.radius = inputvalue;
-
-      let a ='<p style="border-radius: 50%; border: 1px solid black; line-height:'+userCircle.diameter+
-      'px; overflow: hidden; width: '+userCircle.diameter+'px; height:'+userCircle.diameter +
-      'px;">-------------------------------</p> Радиус окружности: R= '+userCircle.radius+'px; <br>'+
-      'Диаметр окружности: D= ' + userCircle.diameter + 'px; <br> Площадь окружности: S= ' + userCircle.areaCircle()+
-       'px2; <br> Длинна окружности: L= ' + userCircle.circumFerence()+ 'px;';
-      out101.innerHTML=a;
+      out101.innerHTML=userCircle.showInfo();
     }
 
     click102.onclick = function () {
-      let a = new HtmlElement('a',false,'More...');
-      a.settingAttribute('href','https://www.lipsum.com/').settingAttribute('target','_blank');
+      let a = new HtmlElement('a',false,'More...');     a.settingAttribute('href','https://www.lipsum.com/').settingAttribute('target','_blank');
       let p = new HtmlElement('p',false,'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'+
        'Etiam sodales lectus eget dictum fringilla. Cras euismod sapien ullamcorper tortor euismod'+
        'suscipit. Sed lobortis magna ut posuere faucibus. Etiam risus eros, elementum eu dolor quis,'+
-       'pellentesque pharetra tellus. Curabitur tortor risus, fringilla ut faucibus sagittis, efficitur eget dui.');
-       p.settingStyle('text-align','justify').endtAttach(a);
+       'pellentesque pharetra tellus. Curabitur tortor risus, fringilla ut faucibus sagittis, efficitur eget dui.'); p.settingStyle('text-align','justify').endtAttach(a);
       let h3 = new HtmlElement('h3',false,'What is Lorem Ipsum?');
-      let img = new HtmlElement('img',true);
-      img.settingStyle('width','100%').settingAttribute('src','lorem-ipsum-fi-2.png').settingAttribute('alt','Lorem ipsum');
-      let divS = new HtmlElement('div',false);
-      divS.settingStyle('width','300px').settingStyle('margin','10px').endtAttach(h3).endtAttach(img).endtAttach(p);
-      let divF = new HtmlElement('div',false);
-      divF.settingAttribute('id','wrapper').settingStyle('display','flex').endtAttach(divS).endtAttach(divS);
-      console.log(divS.getHtml());
+      let img = new HtmlElement('img',true);            img.settingStyle('width','100%').settingAttribute('src','lorem-ipsum-fi-2.png').settingAttribute('alt','Lorem ipsum');
+      let divS = new HtmlElement('div',false);          divS.settingStyle('width','300px').settingStyle('margin','10px').endtAttach(h3).endtAttach(img).endtAttach(p);
+      let divF = new HtmlElement('div',false);          divF.settingAttribute('id','wrapper').settingStyle('display','flex').endtAttach(divS).endtAttach(divS);
       out102.innerHTML=divF.getHtml();
     }
   }
-
-
-
-
-//let div = new HtmlElement('div',false);
-//let hr = new HtmlElement('hr',true);
-//let br = new HtmlElement('br',true);
-//div.settingStyle('id','vsdvdsds').settingStyle('id','vsdvdsds').endAttach(hr).startAttach(br);
-
-//console.log(div.attachment);
-//q.radius=12;
-//console.log(q.circumFerence());
