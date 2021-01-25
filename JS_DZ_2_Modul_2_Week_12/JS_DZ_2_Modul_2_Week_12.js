3eb2a73a697b4f6abbee6cfc39c5d89910e19b34
Eduard <nikolaef.post@gmail.com>
2021-01-25 14:18:37 +0200

    //

--- JS_DZ_2_Modul_2_Week_12/JS_DZ_2_Modul_2_Week_12.js
+++ JS_DZ_2_Modul_2_Week_12/JS_DZ_2_Modul_2_Week_12.js
@@ -5,15 +5,16 @@
       this.color = color;
       this._ink = 100;
       this.text = '';                                                           // поле для текста, который будет напечатан
-      this.temp = [''];                                                         // поле для ненапечатанной части строки: this.temp.[0]- сама часть строки   this.temp.[1] - необходимое количество чернил
-    }
+      this.temp = ['',''];                                                      // поле для ненапечатанной части строки: this.temp.[0]- сама часть строки   this.temp.[2] - необходимое количество чернил
+    }                                                                           //this.temp.[1] - для хранения статуса печати
 
-    markerWrite1(str){                                                          // метод готовит строку доступную к печати
-      for (var i = 0; i < str.length; i++) {
-        (this._ink > 0) ?  this.text += str[i]  : this.temp[0] += str[i];
+    markerWrite1(str){
+      for (var i = 0; i < str.length; i++) {                                    // метод готовит строку доступную к печати
+        (this._ink > 0) ?  this.text += str[i]  : this.temp[0] += str[i];       // и сохраняет информацию о ненапечатаной части строки
         if (str[i] != ' ') this._ink -=0.5;
       }
-      if (this._ink < 0) {this.temp[1] = this._ink; this._ink = 0;}             //и сохраняет информацию о ненапечатаной части строки
+      if (this._ink < 0) {this.temp[2] = this._ink; this._ink = 0;}             // сохраняются данные о необходимом количестве чернил
+      this.writeStatus()
       return this;
     }
 
@@ -29,25 +30,22 @@
     // }
 
     writeStatus(){
-      if (this.temp.length > 1) this.text += '<small> Закончились чернила! Чтобы допечатать оставшийся текст необходимо заправить маркер на'+ this.temp[1] +' едениц.</small><br>';
-      else this.text += '<small>Количество чернил в маркере '+ this.ink +' едениц.</small><br>';
+      if (this.temp.length > 2) this.temp[1] = '<tt> Закончились чернила! Чтобы допечатать оставшийся текст необходимо заправить маркер на'+ this.temp[2] +' едениц.</tt>';
+      else this.temp[1]= '<tt>Печать успешно завершенна.</tt>';
       return this;
     }
 
-
     resumeWrite(){
       if(this._ink>0) {                                                         //
-        this.text = '';
         let str = this.temp[0];
         this.temp[0] = '';
         this.markerWrite1(str);
-     }
+      }
       return this;
     }
 
     print(){
-      this.text = '<p style = "margin-left:15px; color:' + this.color + ';">' + this.text + '</p>';
-      return this;
+      return '<p style = "margin-left:15px; color:' + this.color + ';">' + this.text + '</p>' + this.temp[1];
     }
   }
 
@@ -59,9 +57,10 @@
     set ink(fuel){
       if (Number(fuel)) {
         this._ink = this._ink + Math.abs(fuel);
-        this.temp.length = 1;
+        this.temp.length = 2;
         if (this._ink>100) this._ink = 100;
       }
+      if (this.temp[0].length > 0) this.resumeWrite();
       return this;
     }
   }
@@ -72,16 +71,16 @@
     click101.onclick = function () {
       let str = document.getElementById('input101').value;
       let color = document.getElementById('input102').value;
-      let q = new RefuelingMarker(color).markerWrite1(str).print().writeStatus();
-      out101.innerHTML=q.text;
+      let q = new RefuelingMarker(color).markerWrite1(str);
+      // out101.innerHTML=q.print();
+      // console.log(q, q.temp);
 
-      q.ink = 20;                                                               // дозаправка чернил
-      q.resumeWrite().print().writeStatus();
-      out101.innerHTML+=q.text;
+      q.ink = 26;                                                               // дозаправка чернил
+      out101.innerHTML=q.print();
+      console.log(q, q.temp);
 
       q.ink = 500;                                                               // дозаправка чернил
-      q.resumeWrite().print().writeStatus();
-      out101.innerHTML+=q.text;
-      console.log(q);
+      out101.innerHTML=q.print();
+      console.log(q, q.temp);
     }
   }
