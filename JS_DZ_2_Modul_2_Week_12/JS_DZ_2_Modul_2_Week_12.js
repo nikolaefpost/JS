5df7e5855737c1c69772b1c3f2df720e10387c66
Eduard <yemelyanov.eduard@gmail.com>
2021-01-29 12:28:49 +0200

    JS_DZ_2_Modul_2_Week_12 2/3

--- JS_DZ_2_Modul_2_Week_12/JS_DZ_2_Modul_2_Week_12.js
+++ JS_DZ_2_Modul_2_Week_12/JS_DZ_2_Modul_2_Week_12.js
@@ -68,23 +68,6 @@
 // --------------------------------------------------------------------- 2-e задание --------------------------------------------------------------------------
 class ExtentedDate extends Date {
 
-  numberToText1(objDate){                                                        // метод преобразования числа из цифры в слова
-    let str=[];
-    if (objDate instanceof Date )  str.push(objDate.getDate().toString(), objDate.getMonth());                 // проверка является ли аргумент обьектом Date
-    else { this.rezalt = ' Некорректный ввод данных'; return this; }
-    if (str[0].length>2 || !(Number(str[0])) ) {this.rezalt='Введите число не болбше 2х знаков!'; return this;}
-    if (str[0].length==1) str[0] ='0'+ str[0];
-    let units1 = [
-      ['',' первое',' второе',' третье',' четвертое',' пятое',' шестое',' семдьмое',' восьмое',' девятое'],
-      [' десятое',' одиннадцатое',' двенадцатое',' тринадцатое',' четырнадцатое',' пятнадцатое',' шестнадцатое',' семнадцатое',' восемнадцатое',' девятнадцатое'],
-      ['','',' двадцать(oe)',' тридцать(oe)']
-    ];
-    let units2 = ['января','февраля','марта','апреля','мая','июня','июля','августа','сентября','октября', 'ноября', 'декабря'];
-     (9 < (str[0] % 100) && (str[0] % 100) < 20) ? this.rezalt = units1[1][str[0][1]] : this.rezalt = units1[2][str[0][0]] + units1[0][str[0][1]];
-     this.rezalt+=' '+units2[str[1]];
-    return this;
-  }
-
   numberToText2(){                                                        // метод преобразования числа из цифры в слова
     this.str=[];
     this.str.push(this.getDate().toString(), this.getMonth());
@@ -99,17 +82,21 @@ class ExtentedDate extends Date {
      this.rezalt+=' '+units2[this.str[1]];
     return this;
   }
+
+  testTime(){
+    return (this.getTime() > new Date().getTime());
+  }
+
+  testLeapYear(){
+    this.userYear = this.getFullYear();
+    return (( this.userYear % 4 == 0  && this.userYear % 100 == 0 && this.userYear % 400 == 0) || (this.userYear % 4 == 0  && this.userYear % 100 != 0));
+  }
+
+  nextDate(){
+    this.setDate(this.getDate()+1);
+    return this.toLocaleDateString();
+  }
 }
-
-
-let objDate = new ExtentedDate().numberToText2();
-//let w = new ExtentedDate().numberToText(objDate);
-console.log(objDate.rezalt);
-
-
-
-
-
 
 
 
@@ -120,17 +107,27 @@ console.log(objDate.rezalt);
   window.onload = function () {
 
     click101.onclick = function () {
-      let str = document.getElementById('input101').value;
-      let color = document.getElementById('input102').value;
-      let q = new RefuelingMarker(color).markerWrite1(str);
-       out101.innerHTML=q.print();
-      console.log(q, q.temp);
-
-      q.ink = 26;                                                               // дозаправка чернил
+     let str = document.getElementById('input101').value;
+     let color = document.getElementById('input102').value;
+     let q = new RefuelingMarker(color).markerWrite1(str);
       out101.innerHTML=q.print();
-      console.log(q, q.temp);
+     console.log(q, q.temp);
 
-      q.ink = 99;                                                               // дозаправка чернил
-      console.log(q, q.temp);
+     q.ink = 26;                                                               // дозаправка чернил
+     out101.innerHTML=q.print();
+     console.log(q, q.temp);
+
+     q.ink = 99;                                                               // дозаправка чернил
+     console.log(q, q.temp);
+   }
+
+    click201.onclick = function () {
+      let str = document.getElementById('input201').value.split('.').map((x)=>Number(x));
+      if (!(Number.isInteger(str[0]) && Number.isInteger(str[1]) && Number.isInteger(str[2]))) return out201.innerHTML='data entry is incorrect';           //делаем минимальную проверку на ошибку ввода
+      let objDate = new ExtentedDate(str[2], str[1]-1, str[0] ).numberToText2();
+      out201.innerHTML = objDate.rezalt +'<br>';
+      out201.innerHTML+='является ли дата будущим? - ' + objDate.testTime() +'<br>';;
+      out201.innerHTML+='является ли данный год высокосным? - ' + objDate.testLeapYear() + '<br>';
+      out201.innerHTML+='следущаяя дата будет - ' + objDate.nextDate() +' '+ objDate.nextDate();
     }
   }
