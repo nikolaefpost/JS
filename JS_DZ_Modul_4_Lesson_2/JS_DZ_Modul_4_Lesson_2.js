549872919d2951571d87a24533be9773bb6fcf78
Edard <nikolaef.post@gmail.com>
2021-03-02 09:36:11 +0200

    JS_DZ_Modul_4_Lesson_2 completed

--- JS_DZ_Modul_4_Lesson_2/JS_DZ_Modul_4_Lesson_2.js
+++ JS_DZ_Modul_4_Lesson_2/JS_DZ_Modul_4_Lesson_2.js
@@ -35,15 +35,61 @@
   }
   target_block.addEventListener('mousedown', startResize) ;
 
-//------------------------------------------------------------------------------2е задание --------------------------------------------------------------------
+//------------------------------------------------------------------------------3е задание --------------------------------------------------------------------
 
 
-  pushed.addEventListener('mousemove', pushBlock);
+  pushed.addEventListener('mousedown', startPush);
+
+  function startPush() {
+     document.body.addEventListener('mousemove', pushBlock);
+     document.body.addEventListener('mouseup', pushBlock);
+  }
   function pushBlock() {
     let block = document.getElementById('pushed');
     let shift = event.clientX-axis.offsetLeft-block.offsetWidth/2;
     if (shift<0) shift=0;
     if (shift > (axis.offsetWidth-block.offsetWidth)) shift = axis.offsetWidth-block.offsetWidth;
     block.style.left = shift;
+    if (event.type == 'mouseup') {
+      document.body.removeEventListener('mousemove', pushBlock) ;
+      document.body.removeEventListener('mouseup', pushBlock) ;
+    }
   }
+
+//------------------------------------------------------------------------------4е задание --------------------------------------------------------------------
+
+  let arrImg = ['adidas_logo_icon_181260.png', 'apple_logo_icon_181323.png', 'bitcoin_logo_icon_181258.png', 'intel_logo_icon_181251.png'];
+  galleryCarousel(arrImg);
+
+  function galleryCarousel(arrImg) {
+    const width = 256;
+    let list = carousel.querySelector('ul');
+    for (let i = 0; i < arrImg.length; i++) {
+      let li = document.createElement('li');
+      let img = document.createElement('img');
+      img.setAttribute('src', arrImg[i]);
+      li.append(img);
+      list.append(li);
+    }
+    let listElems = carousel.querySelectorAll('li');
+    let left = carousel.querySelector('.left');
+    let right = carousel.querySelector('.right');
+    let position = 0;
+
+    left.onclick = function() {
+      if(right.classList.contains('hovered')) right.classList.remove('hovered');
+      position += width;
+      if (position >= 0) { position = 0; left.classList.add('hovered'); }
+      list.style.marginLeft = position + 'px';
+    }
+
+    right.onclick = function() {
+      if(left.classList.contains('hovered')) left.classList.remove('hovered');
+      position -= width;
+      if (position <= -width * (listElems.length - 1)) { position = -width * (listElems.length - 1); right.classList.add('hovered'); }
+      list.style.marginLeft = position + 'px';
+    }
+  }
+
+
   }
