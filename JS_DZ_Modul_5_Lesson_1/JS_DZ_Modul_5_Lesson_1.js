bb24abc46da858c30db223fb0e5faf392ea77d13
Edard <nikolaef.post@gmail.com>
2021-03-04 16:02:10 +0200

    \\\\

--- JS_DZ_Modul_5_Lesson_1/JS_DZ_Modul_5_Lesson_1.js
+++ JS_DZ_Modul_5_Lesson_1/JS_DZ_Modul_5_Lesson_1.js
@@ -2,32 +2,20 @@ window.onload = function () {
 
   function addMessage(article_class) {
     let form = document.forms[0];
-    let s = document.querySelector(article_class)
-
-
-
+    let article = document.querySelector(article_class)
     return function () {
       let user_name = form.elements.name.value;
       let user_message = form.elements.message.value;
-      var container = s.cloneNode(true);
-
+      var container = article.cloneNode(true);
       let span = container.getElementsByTagName('span');
       span[1].innerText = user_name;
       span[2].innerText = (new Date()).toLocaleString();
       container.children[2].innerText = user_message;
       main.prepend(container);
     }
-
-
   }
 
-document.onclick = function () {
-  if (event.target.classList.contains("x"))
-  event.target.parentElement.remove();
-  console.log(event.target.parentElement);
-
-
-}
+  document.onclick = function () { if (event.target.classList.contains("x"))  event.target.parentElement.remove();}
   button.onclick = addMessage('.container');
 
 
