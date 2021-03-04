54e0af04236071365b2e4b3f1ebdb8a3ab8f9aed
Edard <nikolaef.post@gmail.com>
2021-03-04 16:01:40 +0200

    \\\

--- JS_DZ_Modul_5_Lesson_1/JS_DZ_Modul_5_Lesson_1.js
+++ JS_DZ_Modul_5_Lesson_1/JS_DZ_Modul_5_Lesson_1.js
@@ -1,25 +1,35 @@
 window.onload = function () {
-  let form = document.forms[0];
-  let user_name = form.elements.name.value;
-  let user_message = form.elements.message.value;
-  // let div = document.createElement('div');
-  // div.classList.add('container');
-  // let x = document.createElement('span');
-  // x.setAttribute("id", "x");
-  // x.innerText = 'x';
-  // div.prepend(x);
-  // let p1 = document.createElement('p');
-  // p1.classList.add('header', 'border_bottom');
-  var container = document.querySelector('.container').cloneNode(true);
-  let span = container.getElementsByTagName('span');
-  span[1].innerText = user_name;
-  span[2].innerText = (new Date()).toLocaleString();
-  container.children[2].innerText = user_message;
 
-  button.onclick = function () {
-    main.prepend(container);
+  function addMessage(article_class) {
+    let form = document.forms[0];
+    let s = document.querySelector(article_class)
+
+
+
+    return function () {
+      let user_name = form.elements.name.value;
+      let user_message = form.elements.message.value;
+      var container = s.cloneNode(true);
+
+      let span = container.getElementsByTagName('span');
+      span[1].innerText = user_name;
+      span[2].innerText = (new Date()).toLocaleString();
+      container.children[2].innerText = user_message;
+      main.prepend(container);
+    }
+
+
   }
 
+document.onclick = function () {
+  if (event.target.classList.contains("x"))
+  event.target.parentElement.remove();
+  console.log(event.target.parentElement);
 
-  console.log(container.children[2]);
+
+}
+  button.onclick = addMessage('.container');
+
+
+
 }
