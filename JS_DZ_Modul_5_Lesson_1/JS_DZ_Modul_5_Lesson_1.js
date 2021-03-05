49a8a72c9cbda5e7f6df9a92db0156dbfe4d94f6
Edard <nikolaef.post@gmail.com>
2021-03-05 19:52:50 +0200

    JS_DZ_Modul_5_Lesson_1 comleted

--- JS_DZ_Modul_5_Lesson_1/JS_DZ_Modul_5_Lesson_1.js
+++ JS_DZ_Modul_5_Lesson_1/JS_DZ_Modul_5_Lesson_1.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-
+//------------------------------------------------------------------------------1е задание --------------------------------------------------------------------
   function addMessage(article_class) {
     let form = document.forms[0];
     let article = document.querySelector(article_class)
@@ -17,8 +17,34 @@ window.onload = function () {
 
   document.onclick = function () { if (event.target.classList.contains("x"))  event.target.parentElement.remove();}
   button.onclick = addMessage('.container');
+//------------------------------------------------------------------------------2е задание --------------------------------------------------------------------
+  btn1.addEventListener('click', buttonHandling1);
+  btn2.addEventListener('click', buttonHandling2);
+  btn0.addEventListener('click', buttonHandling0);
 
-let question_form = document.forms[1];
-console.log(question_form);
+  function buttonHandling0 () {
+    btn0.hidden = true;
+    document.forms[1].hidden = false;
+  }
 
+  function buttonHandling1 () {
+    if (!document.forms[1].planet.value) {
+      alert('Сделайте ваш выбор!');
+      return;
+    }
+    document.forms[1].hidden = true;
+    document.forms[2].hidden = false;
+  }
+
+  function buttonHandling2() {
+    if (!document.forms[2].language.value) {
+      alert('Сделайте ваш выбор!');
+      return;
+    }
+    let i=0;
+    document.forms[2].hidden = true;
+    if (document.forms[1].planet.value==0) i++;
+    if (document.forms[2].language.value==1) i++;
+    out512.innerText = 'Congratulations! Rezult: '+i+' correct answers to '+ (document.forms.length-1)+' questions';   //-1 форма из предыдущего задания)
+  }
 }
