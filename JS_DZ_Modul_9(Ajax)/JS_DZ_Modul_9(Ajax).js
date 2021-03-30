efd2d442a269046d4b71a038c1fe11633a3fc360
Eduard <nikolaef.post@gmail.com>
2021-03-30 14:15:16 +0300

    JS_DZ_Modul_9(Ajax)

--- JS_DZ_Modul_9(Ajax)/JS_DZ_Modul_9(Ajax).js
+++ JS_DZ_Modul_9(Ajax)/JS_DZ_Modul_9(Ajax).js
@@ -4,16 +4,21 @@ window.onload = function () {
     constructor(url, apikey) {
       this.url = url;
       this.apikey = apikey;
-      this.momie;
+      this.temp='';
     }
+
     async search (title, type, page_search) {
-      this.request = await fetch(this.url + '?s=' + title + '&plot=full&page=' + page_search + '&type=' + type + '&apikey=' + this.apikey)
-      .then(search=> search.json());
-      return this.request.Search;
+      let request = await fetch(this.url + '?s=' + title + '&plot=full&page=' + page_search + '&type=' + type + '&apikey=' + this.apikey);
+      let json = await request.json();
+      return json.Search;
+    }
+
+    async searchDetails (imbd_id) {
+      let request = await fetch(`http:www.omdbapi.com/?i=${imbd_id}&plot=full&apikey=${this.apikey}`)
+      let json = await request.json();
+      return json;
     }
   }
-
-  let requestT = new MediaQuery1('http://www.omdbapi.com/','ab776285');
 
 
   class ShowFilm {
@@ -21,128 +26,88 @@ window.onload = function () {
       this.page_search = 1;
       this.page = 1;
       this.arrFilms = [];
-      this.temp=0;
+      this.temp=[];
     }
 
-    async prepeaFilm (request) {
+    async  prepeaFilm (request) {
+
+      if (this.temp[0] == this.title && this.temp[1] == this.type && this.temp[2] == this.page_search) return;
+      preload1.hidden = false;
       this.title = document.forms.media_content.title.value;
       this.type = document.forms.media_content.type.value;
-      request.search(this.title, this.type, this.page_search).then((films)=>{
-        this.arrFilms = this.arrFilms.concat(films);
-
-         (this.page>1)? this.addFilm(): this.addFilm();
-      });
+      this.temp = [this.title, this.type, this.page_search];
+      let films = await requestT.search(this.title, this.type, this.page_search);
+      this.arrFilms = this.arrFilms.concat(films);
+      this.addFilm();
       more.hidden = false;
-      more.addEventListener('click', ()=>{this.page++; this.addFilm()});
+      preload1.hidden = true;
     }
 
     addFilm(){
       if (out1.children) out1.innerHTML ='';
-      console.log(this.page);
-      console.log(this.arrFilms.length);
-
-
-      // this.page++;
-      console.log(this.arrFilms.length/5 < this.page);
-
       if (this.arrFilms.length/5 < this.page) {
         this.page_search++;
-        // this.page--;
-        console.log(this.page);
         this.prepeaFilm (requestT)
         return;
       }
-
+      if (zag.children) zag.innerHTML ='';
+      zag.innerHTML+='<h3 style="text-align:center;">Films:</h3>';
 
       for (let i = 0; i < this.page*5; i++) {
         let clone = document.querySelector('.example').cloneNode(true);
-            clone.classList.remove('hidden');
-            let img = clone.getElementsByClassName('poster')[0];
-            img.src = this.arrFilms[i].Poster;
-          let  name_ = clone.getElementsByClassName('name')[0];
-            name_.innerText = this.arrFilms[i].Title;
-            let year = clone.getElementsByClassName('year')[0];
-            year.innerText = this.arrFilms[i].Year;
-            let imbd_id = clone.getElementsByClassName('imbd_id')[0];
-            imbd_id.innerText = this.arrFilms[i].imdbID;
-            let button_details = clone.getElementsByClassName('button_details')[0];
-            // button_details.addEventListener('click', mediaQueryDetails);
-            out1.appendChild(clone);
-      }
+        clone.classList.remove('hidden');
+        let img = clone.getElementsByClassName('poster')[0];
+        img.src = this.arrFilms[i].Poster;
+        let name_ = clone.getElementsByClassName('name')[0];
+        name_.innerText = this.arrFilms[i].Title;
+        let year = clone.getElementsByClassName('year')[0];
+        year.innerText = this.arrFilms[i].Year;
+        let imbd_id = clone.getElementsByClassName('imbd_id')[0];
+        imbd_id.innerText = this.arrFilms[i].imdbID;
+        let button_details = clone.getElementsByClassName('button_details')[0];
+        button_details.addEventListener('click', ()=>this.showFilmDetails(imbd_id.innerText));
 
-      console.log(this.page);
-      console.log(this.arrFilms.length);
+        out1.appendChild(clone);
+        clone.scrollIntoView(top)
+      }
     }
 
+    async showFilmDetails(id) {
+      preload2.hidden = false;
+      poster.hidden = true;
+      text.style.display = 'none';
+      let film = await requestT.searchDetails(id);
+      preload2.hidden = true;
+      poster.hidden = false;
+      text.style.display = '';
+      area_div.classList.add('hystmodal__opened');
+      film_details.classList.remove('hidden');
+      id_title.innerText = film.Title;
+      released.innerText = film.Released;
+      genre.innerText = film.Genre;
+      country.innerText = film.Country;
+      director.innerText = film.Director;
+      writer.innerText = film.Writer;
+      actors.innerText = film.Actors;
+      awards.innerText = film.Awards;
+      poster.src = film.Poster;
+      modal_button.addEventListener('click', ()=>this.closeFilmDetails());
+      document.body.addEventListener('keydown', ()=>this.closeFilmDetails());
+    }
 
+    closeFilmDetails(){
+      if (event.type == "click" || event.key == "Escape") {
+        area_div.classList.remove('hystmodal__opened');
+        film_details.classList.add('hidden');
+      }
+
+    }
   }
-button.addEventListener('click', ()=>new ShowFilm().prepeaFilm(requestT));
-  // let fils = new ShowFilm().prepeaFilm(new MediaQuery1('http://www.omdbapi.com/','ab776285'));
 
 
-
-
-
-
-
-
-
-  // function mediaQueryDetails() {
-  //   let request = new XMLHttpRequest();
-  //   let imbd_id = event.target.parentElement.getElementsByClassName('imbd_id')[0];
-  //   let str = "http://www.omdbapi.com/?i="+ imbd_id.innerText+"&plot=full&apikey=ab776285";
-  //   request.open("GET", str);
-  //   let film;
-  //   request.onload = function(){
-  //     if(request.status === 200){
-  //       film = JSON.parse(request.response);
-  //       showFilmDetails(film)
-  //     }
-  //   }
-  //   request.send(film);
-  // }
-  //
-  // function showFilmDetails(film) {
-  //   film_details.classList.remove('hidden');
-  //   id_title.innerText = film.Title;
-  //   released.innerText = film.Released;
-  //   genre.innerText = film.Genre;
-  //   country.innerText = film.Country;
-  //   director.innerText = film.Director;
-  //   writer.innerText = film.Writer;
-  //   actors.innerText = film.Actors;
-  //   awards.innerText = film.Awards;
-  //   poster.src = film.Poster;
-  // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  let requestT = new MediaQuery1('http://www.omdbapi.com/','ab776285');
+  let object = new ShowFilm();
+  button.addEventListener('click', ()=>object.prepeaFilm(requestT));
+  more.addEventListener('click', ()=>{object.page++; object.addFilm()});
 
 }
