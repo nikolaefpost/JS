b756adaa80acec50168c6a99c6753a1aaa5cec82
Eduard <nikolaef.post@gmail.com>
2021-04-19 17:27:09 +0300

    ...

--- JS_DZ_Modul_14(Patterns)/JS_DZ_Modul_14(Patterns).js
+++ JS_DZ_Modul_14(Patterns)/JS_DZ_Modul_14(Patterns).js
@@ -47,10 +47,7 @@
       constructor(tagName) {
         this.result = new Element(tagName);
       }
-      // create(){
-      //   this.result = new Element(tagName);
-      //   return this;
-      // }
+
       withClass(className) {
         this.result.addClass(className)
         return this;
@@ -77,13 +74,15 @@
       }
     }
 //------------------------------------------------------------ clients code -----------------------------------------------------------------------------
-    let q1 = new DomBuilder('span').withClass('red').withContent('1-span');
-    let q2 = new DomBuilder('span').withClass('red').withContent('2-span');
-    let img = new DomBuilder('img').withAttribute('src', '54501-ferrari_krasnyj_sportkar_vid_szadi.jpg').withStyle('float', 'left');
-    let q = new DomBuilder('p').withClass('main').withClass('red1').withId('idName').withStyle('color', 'blue')
-    .withAttribute('data-number', 1).withContent('some text').withChild(q1).withChild(q2).withChild(img).result;
-    out.append(q.element);
-    console.log(q.print());
+
+    let text ='Ferrari is one of the world’s most admired luxury sports car automakers. The company officially launched in 1947. However, the automaker’s legendary founder and namesake Enzo Ferrari was involved in the industry long before then. About Enzo.  Born in Modena, Italy in 1898, Enzo Ferrari started out as a race car driver. In the 1920s, he was made a driver with Alfa Romeo and won several racing awards including the 2nd Circuito di Modena. In 1929, Ferrari founded the Scuderia Ferrari, now the official race car division for Ferrari. It began, however, as a division of Alfa that specialized in preparing race cars to gentlemen drivers. This was at a time when sports car racing was really taking off as an activity for the rich and famous, who were really the only ones who could afford it. In 1931, Enzo finished in second place at the Circuito Tre Province, which would be his last time competing as a driver. He wanted to focus on his family and the birth of his first son Alfredo more commonly called Dino. However, he still continued working with Scuderia and building cars for Alfa until 1939. In September of that year, he left his position as Head of Alfa Corse to form his own car company Auto Avio Costruzioni in his hometown of Modena, Italy. As a condition of his departure, he was required not to use the Ferrari name in association with any of his cars or races for at least four years after. Honoring the agreement, the company name wasn’t changed to Auto Costruzioni Ferrari until 1957, and first car to bare the name Ferrari was released the Ferrari 125 S.';
+    let p = new DomBuilder('p').withClass('text').withContent(text);
+    let h2 = new DomBuilder('h2').withClass('red').withContent('Ferrari');
+    let img = new DomBuilder('img').withAttribute('src', '54501-ferrari_krasnyj_sportkar_vid_szadi.jpg').withStyle('float', 'left').withStyle('margin-right', '30px').withAttribute('width', '250px');
+    let div = new DomBuilder('div').withClass('main').withClass('shadow').withId('idName')
+    .withAttribute('data-number', 1).withChild(img).withChild(h2).withChild(p).result;
+    out.append(div.element);
+    console.log(div.print());
 
 
   }
