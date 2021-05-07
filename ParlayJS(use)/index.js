66880795a8f53cd92cfb2a94df05750efc84d3e4
Eduard <nikolaef.post@gmail.com>
2021-05-07 20:03:02 +0300

    parsley +

--- ParlayJS(use)/index.js
+++ ParlayJS(use)/index.js
@@ -8,14 +8,28 @@ $(function () {
     event.preventDefault();
     let user = $('.username').val();
     if  (user == 'admin' || user == 'user' || user == 'test'){
-    new  WarningMessage('.username'); return false;
+      new  WarningMessage('.username'); return false;
     }
     if ($('.password1').val()!=$('.password2').val()) {
       new  WarningMessage('.password2'); return false;
     }
     let complexity = testPasword($('.password1').val())
     let email = $('.email').val();
-    new View(user, email, complexity)
+    new RegistredView(user, email, complexity)
+  });
+});
+
+$(function () {
+  $('.user_info_form').parsley().on('field:validated', function() {
+    var ok = $('.parsley-error').length === 0;
+    $('.bs-callout-info').toggleClass('hidden', !ok);
+  })
+  .on('form:submit', function() {
+    event.preventDefault();
+    if (new Date($(".birthday").val()).getFullYear()<1900) {
+      new  WarningMessage('.birthday'); return false;
+    }
+    new InfoView($(".fullname").val(), $(".avatar").val(), $(".birthday").val(), $('input[name="gender"]:checked').val(), $('.subscribe').is(':checked'))
   });
 });
 
@@ -35,23 +49,6 @@ class WarningMessage {
     });
   }
 }
-
-// function warningMessage(class_){
-//  let hintText = $(class_).data( "tooltip" );
-//  if (!hintText) return;
-//
-//  let hint = document.createElement('div');
-//  hint.className = 'tooltip';
-//  hint.innerHTML = hintText;
-//  $(class_).parent().append(hint);
-//  $(class_).focus()
-//  $(class_).blur( function () {
-//    if (hint) {
-//      hint.remove();
-//      hint = null;
-//    }
-//  });
-//  }
 
  function testPasword(str) {
    let n=0, l_s=0, l_b=0, s=0
@@ -74,7 +71,7 @@ class WarningMessage {
    else return 'normaly';
  }
 
- class View {
+ class RegistredView {
    constructor(username, email, complexity) {
        let rezultContainer = document.createElement("div");
          rezultContainer.innerHTML = document.getElementById('rezult').innerHTML
@@ -82,20 +79,25 @@ class WarningMessage {
          .replace(/{{email}}/, email)
          .replace(/{{complexity}}/, `******* (${complexity})`)
          let result = rezultContainer.children[0];
+         rezult_list.innerHTML = '';
          rezult_list.append(result);
    }
  }
 
+ class InfoView {
+   constructor(fullname, avatar, birthday, gender, subscribe) {
+     this.gender = ['Female', 'Male'];
+     this.subscribe = ['No', 'Yes'];
 
-
- $(function () {
-   $('.user_info_form').parsley().on('field:validated', function() {
-     var ok = $('.parsley-error').length === 0;
-     $('.bs-callout-info').toggleClass('hidden', !ok);
-     $('.bs-callout-warning').toggleClass('hidden', ok);
-   })
-   .on('form:submit', function() {
-     event.preventDefault();
-     
-   });
- });
+       let rezultContainer = document.createElement("div");
+         rezultContainer.innerHTML = document.getElementById('rezult_info').innerHTML
+         .replace(/{{fullname}}/, fullname)
+         .replace(/{{avatar}}/, avatar)
+         .replace(/{{birthday}}/, birthday)
+         .replace(/{{gender}}/, this.gender[gender])
+         .replace(/{{subscribe}}/, subscribe? 'Yes': 'No')
+         let result = rezultContainer.children[0];
+         rezult_info_list.innerHTML = '';
+         rezult_info_list.append(result);
+   }
+ }
